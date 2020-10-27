dba962d25328a5066d6bd28513535b288f9a9f73
RodrigoGCoelhoo <rodrigogc4@al.insper.edu.br>
2020-10-27 00:34:11 -0300

    Commit inicial

---
+++ milk-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return <Button color="secondary">Insper Vacathon</Button>;
+  
+}
+
+export default App;
---
+++ milk-app/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAakGtZnsBWA5wtMVDLnSJMJi0Yqnb_-g",
+  authDomain: "vacathon-1ba21.firebaseapp.com",
+  databaseURL: "https://vacathon-1ba21.firebaseio.com",
+  projectId: "vacathon-1ba21",
+  storageBucket: "vacathon-1ba21.appspot.com",
+  messagingSenderId: "586328858963",
+  appId: "1:586328858963:web:6ccdf03df5e8a88162c1a6",
+  measurementId: "G-F6R7EXKE9J"
+};
+
+firebase.default.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
