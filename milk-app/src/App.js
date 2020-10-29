9abaaf91576e9808279441f7c36dc3e61c0e2dd6
RodrigoGCoelhoo <rodrigogc4@al.insper.edu.br>
2020-10-29 15:24:02 -0300

    Correcao pequena

--- milk-app/src/App.js
+++ milk-app/src/App.js
@@ -16,14 +16,14 @@ import Tela_leite_1 from './Tela_leite_1'
 import Tela_leite_2 from './Tela_leite_2'
 import Tela_cadastro_vaca from './Tela_cadastro_vaca'
 import tela_relatorio from './Tela_relatorio'
+import Tela_financeiro_valores from './Tela_financeiro_valores'
 
 function App() {
   return (
     <Router>
       <div className="App">
-      {window.location.pathname!=='/login' ? <Header/>:null}
         <Switch>
-          <Route path="/login" exact component={Login}/>
+          <Route path="/" exact component={Login}/>
           <Route path="/tela_leite_1" exact component={Tela_leite_1}/>
           <Route path="/tela_leite_2" exact component={Tela_leite_2}/>
           <Route path="/tela_cadastro_vaca" exact component={Tela_cadastro_vaca}/>
